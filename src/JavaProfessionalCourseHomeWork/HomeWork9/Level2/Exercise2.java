17ac6c5412f37735a4d5c481548569ce5d8dfb95
dickchest <dickchest@gmail.com>
2023-02-16 19:35:44 +0100

    Edit Home Work 9

--- src/JavaProfessionalCourseHomeWork/HomeWork9/Level2/Exercise2.java
+++ src/JavaProfessionalCourseHomeWork/HomeWork9/Level2/Exercise2.java
@@ -13,16 +13,17 @@ public class Exercise2 {
     public static void main(String[] args) {
 
         List<Integer> array = Arrays.asList(1, 2, 3, 4, 5, 6);
-        List<Integer> resultArray = new ArrayList<>();
-        int k = 1;
         int size = array.size();
-
-        for (int i = 0; i < size; i++) {
-//            resultArray.add(array.get(size + k) % size);
-            System.out.println((i + k));
-
+        List<Integer> resultArray = new ArrayList<>();
+        for (Integer i : array) {
+            resultArray.add(i);
         }
+        int k = 3;
         System.out.println(resultArray);
+        for (int i = 0; i < size; i++) {
+            array.set((i + k) % size, resultArray.get(i));
+        }
+        System.out.println(array);
 
 
     }
